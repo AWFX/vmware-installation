c5060c637ea89e7416edbb3753621c4a283b637a
Artyom <anon117788@gmail.com>
2024-09-20 14:19:53 +0500

    Release

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ read ans
 if ((ans == Y))
 then
  sudo dnf install kernel-devel kernel-headers gcc make patch wget
-
+ wget https://dl.comss.org/download/VMware-Workstation-Full-17.5.2-23775571.x86_64.bundle
+ chmod +x VMware-Workstation-Full-17.5.2-23775571.x86_64.bundle
  sudo ./VMware-Workstation-Full-17.5.2-23775571.x86_64.bundle
  
  sudo patch /usr/src/kernels/$(uname -r)/arch/x86/include/asm/timex.h vmware-timex-h.patch
